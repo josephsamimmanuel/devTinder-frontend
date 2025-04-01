ff9bdf9f7cd9d0abeda17ff738ee007f218aad7e
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-01 22:21:05 +0530

    Update BASE_URL in constants.js to point to API endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:7777"
+export const BASE_URL="/api"
 export const BACKGROUND_IMAGE="https://img.pikbest.com/backgrounds/20190422/blue-technology-line-background-promotion-banner_1813116.jpg!w700wp"
