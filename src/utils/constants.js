40979b2c0b3971ff9a0a70f5adb0d29923b988a5
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-10 22:27:30 +0530

    Make BASE_URL dynamic based on environment; use localhost for development and default to API for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL="/api"
+// Make base url dynamic
+export const BASE_URL= location.hostname === 'localhost' ? 'http://localhost:7777' : '/api'
 export const BACKGROUND_IMAGE="https://img.pikbest.com/backgrounds/20190422/blue-technology-line-background-promotion-banner_1813116.jpg!w700wp"
