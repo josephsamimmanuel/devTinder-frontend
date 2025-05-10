8212b44224dbcd78040c796afef60539f1dc06cf
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-05-10 08:54:42 +0530

    Fix BASE_URL in constants.js to correctly point to the production URL, ensuring proper API access in non-localhost environments.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // Make base url dynamic
-export const BASE_URL= location.hostname === 'localhost' ? 'http://localhost:7777' : '/api || https://dev-tinder-service.onrender.com'
+export const BASE_URL= location.hostname === 'localhost' ? 'http://localhost:7777' : 'https://dev-tinder-service.onrender.com'
 export const BACKGROUND_IMAGE="https://img.pikbest.com/backgrounds/20190422/blue-technology-line-background-promotion-banner_1813116.jpg!w700wp"
